43b238f7874ad2c72a0ee63ef90e29d7a2232e12
thamiresoliveiragomes <thamiresoliveira1004@gmail.com>
2019-09-24 10:54:25 -0300

    fix checkbox

--- src/main.js
+++ src/main.js
@@ -12,7 +12,10 @@ const btnSearch = document.getElementById("search-btn");
 const btnAlphabeticalOrder = document.getElementById("alphabetical-order");
 const data = app.getData(window.RICKANDMORTY.results);
 
-card.render(data.sort(randOrd), main);
+const start = () => {
+  card.render(data.sort(randOrd), main);
+  checkbox(data);
+};
 
 const filterRepeated = (arr, condition) => {
   const list = [];
@@ -35,33 +38,40 @@ createDropdownMenu(filterRepeated(data, "location"), dropdownLocation);
 createDropdownMenu(filterRepeated(data, "status"), dropdownStatus);
 
 dropdownStatus.addEventListener("click", function(e) {
-  card.render(app.filter(data, "status", e.target.id).sort(randOrd), main);
+  const filterStatus = app.filter(data, "status", e.target.id);
+  card.render(filterStatus.sort(randOrd), main);
   labelStatus.innerHTML = e.target.id;
   labelOrigin.style.cssText = "width: auto;";
   labelOrigin.innerHTML = "Origin";
   labelLocation.innerHTML = "Last location";
   filterInfo.innerHTML = `SHOWING ONLY CHARACTERS WITH STATUS <span>${e.target.id.toUpperCase()}</span>`;
   statistics.innerHTML = `${parseInt(app.getStatistics(data, "status", e.target.id))}% of the characters ${e.target.id === "unknown" ? "have status unkown" : `are ${e.target.id.toLowerCase()}`}`;
+  
+  checkbox(filterStatus);
 });
 
 dropdownOrigin.addEventListener("click", function(e) {
-  card.render(app.filter(data, "origin", e.target.id).sort(randOrd), main);
+  const filterOrigin = app.filter(data, "origin", e.target.id);
+  card.render(filterOrigin.sort(randOrd), main);
   labelOrigin.innerHTML = e.target.id;
   labelOrigin.style.cssText = "width: auto;";
   labelStatus.innerHTML = "Status";
   labelLocation.innerHTML = "Last location";
   filterInfo.innerHTML = `SHOWING ONLY CHARACTERS FROM <span>${e.target.id.toUpperCase()}</span>`;
   statistics.innerHTML = `${app.getStatistics(data, "origin", e.target.id).toFixed(2)}% of the characters are from ${e.target.id.toLowerCase()}`;
+  checkbox(filterOrigin);
 });
 
 dropdownLocation.addEventListener("click", function(e) {
-  card.render(app.filter(data, "location", e.target.id).sort(randOrd), main);
+  const filterLocation = app.filter(data, "location", e.target.id);
+  card.render(filterLocation.sort(randOrd), main);
   labelLocation.innerHTML = e.target.id;
   labelOrigin.style.cssText = "width: auto;";
   labelStatus.innerHTML = "Status";
   labelOrigin.innerHTML = "Origin";
   filterInfo.innerHTML = `SHOWING ONLY CHARACTERS AT <span>${e.target.id.toUpperCase()}</span>`;
   statistics.innerHTML = `${app.getStatistics(data, "location", e.target.id).toFixed(2)}% of the characters are at ${e.target.id.toLowerCase()}`;
+  checkbox(filterLocation);
 });
 
 btnSearch.addEventListener("click", function(e) {
@@ -75,6 +85,13 @@ function randOrd() {
   return (Math.round(Math.random()));
 }
 
-btnAlphabeticalOrder.addEventListener("click", function(e) {
-  card.render(app.alphabeticalOrder(data), main);
-});
+const checkbox = arr => {
+  btnAlphabeticalOrder.checked = false;
+  btnAlphabeticalOrder.addEventListener("click", function(e) {
+    if (btnAlphabeticalOrder.checked) {
+      card.render(app.alphabeticalOrder(arr), main);
+    } else {
+      card.render(arr.sort(randOrd), main);
+    };
+  });
+};
